17db2ce48494d19605130bdb4d8ee1d6958b3589
Oksana Romanenko <roa86tmk09@gmail.com>
2020-04-09 22:09:04 +0400

    class and prototype

--- main.js
+++ main.js
@@ -1,116 +1,31 @@
-// ПОСТРОЕНИЕ ТРЕУГОЛЬНИКА В ЦИКЛЕ
-
-let triagle = '#';
-while(triagle.length <= 7){
-	console.log(triagle);
-	triagle += '#';	
-}
-
-// FizzBuzz
-
-for(let i = 1; i<=100; i++){
-	if(i%3==0 && i%5==0){
-		console.log('FizzBuzz');
-	} else if(i%3==0){
-		console.log('Fizz');
-	} else if(i%5==0){
-		console.log('Buzz');
-	} else {console.log(i);
+var obj = {
+	x : 10,
+	foo : function (){
+		console.log(run);
 	}
 }
 
+var newObj = Object.create(obj);    // создаём новый объект на базе старого
 
-//ШАХМАТНАЯ ДОСКА
-let size = 5;
-let chess;
-let result = ' ';
-//console.log(chess.length);
-for(let i = 0; i < size; i++){
-	if (i == 0){
-		chess='#';
-		while(chess.length < size - 1){
-			if(chess.length % 2 !== 0){
-				chess += ' ';
-			} else {
-				chess += '#';
-			}
-		}
-		chess += '\n';
-	 result += chess;
-	} else if (i % 2 == 0){
-		chess=' ';
-		while(chess.length < size){
-			if(chess.length % 2 !== 0){
-				chess += '#';
-			} else {
-				chess += ' ';
-			}
-		}
-		chess += '\n';
-	 result += chess;
+newObj.t = ’too’   // добавляем новое свойство к новому объекту
 
-	} else {
-		chess = '#';
-		while(chess.length < size){
-			if(chess.length % 2 !== 0){
-				chess += ' ';
-			} else {
-				chess += '#';
-			}
-		}
-		chess += '\n';
-	result += chess;
-	}
+newObj.hasOwnProperty(’t’)  // принадлежит ли свойство t объекту newObj
+obj.isPrototypeOf(newObj).  // является ли obj прототипом для newObj
+
+//	Перебор объекта for in   and    for of
+
+//	this - это ссылка на какой-то объект
+
+var person = {
+	name : 'Alex',
+	family :  'Bobikov',
+	showPersons(){
+		return `Эту персону зовут ${this.name} ${this.family}.`;
+	},
 }
-console.log(result);
+console.log(person.showPersons());
+//showPersons.bind(person)();
 
-// МИНИМУМ
-/*
-function min(val1, val2){
-	return (val1 < val2) ? val1 : val2;
-}
-alert(min(8, 15));
-*/
-
-//МИНИМУМ ++
-
-let num1 = Number(prompt('введите первое число для сравнения'));
-let num2 = Number(prompt('введите второе число для сравнения'));
-function min(val1, val2){
-	return (val1 < val2) ? val1 : val2;
-}
-alert(min(num1, num2));
-
-
-// РЕКУРСИЯ
-/*
-function isEven(n){
-	if(n < 0) n = -n;
-	if(n === 0) {
-		return true;
-	} else if (n === 1) {
-		return false;
-	} else {
-		return isEven(n-2);  
-	}
-}
-console.log(isEven(2));
-*/
-
-// ПОДСЧЕТ БУКВ
-/*
-let countChar = function (str, letter){
-	let counter = 0;
-	for(let i = 0; i < str.length; i++ ){
-		if(str[i] === letter) counter += 1;
-	}
-	return counter;
-}
-console.log(countChar('Мой муж лучший муж на свете', 'у'));
-
-function countBs(str){
-	return countChar (str, "В"); 
-}
-
-console.log(countBs('Вышел Вова Во дВор.'));
-*/
+//function showPersons(){
+//	console.log(`Эту персону зовут ${this.name} ${this.family}.`);
+//}
